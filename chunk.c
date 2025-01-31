75b4ac8a132b1a8c3ab862726ef4f0e20a34f89c
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-01-31 17:17:40 +0100

    -refactor: chunk

--- chunk.c
+++ chunk.c
@@ -6,13 +6,14 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:54:28 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/01/29 16:14:22 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/01/31 17:13:23 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stddef.h>
 #include "libft/ft_printf.h"
+
 
 int determine_chunks(int size)
 {
@@ -26,33 +27,11 @@ int determine_chunks(int size)
         return size / 50; // Пример для очень больших стэков
 }
 
-// int my_log(int n)
-// {
-    // int log = 0;
-    // while (n > 1)
-    // {
-        // n /= 2;
-        // log++;
-    // }
-    // return log;
-// }
-// 
-// int determine_chunks(int size)
-// {
-    // if (size <= 20)
-        // return 2; // Для маленьких стэков
-    // else if (size <= 100)
-        // return 5; // Для средних стэков
-    // else if (size <= 500)
-        // return (int)(my_log(size) * 2); // Логарифмический расчёт для больших стэков
-    // else
-        // return (int)(my_log(size) * 3); // Больше чанков для очень больших стэков
-// }
-
 // Функция для определения размера чанка
 int get_chunk_size(int size, int chunks)
 {
-    return size / chunks + (size % chunks != 0 ? 1 : 0);
+    // return size / chunks + (size % chunks != 0 ? 1 : 0);
+    return (size + chunks - 1) / chunks;
 }
 
 // Функция для определения границ чанка
@@ -119,11 +98,42 @@ void move_to_top(t_stack *a, int direction, int moves)
     }
 }
 
+void rotate_to_max(t_stack *b)
+{
+    t_node *current = b->top;
+    t_node *max = current;
+    int pos = 0;
+    int max_pos = 0;
+    
+    while (current)
+    {
+        if (current->index > max->index)
+        {
+            max = current;
+            max_pos = pos;
+        }
+        current = current->next;
+        pos++;
+    }
+    
+    if (max_pos <= b->size / 2)
+    {
+        while (max_pos--)
+            rb(b);
+    }
+    else
+    {
+        max_pos = b->size - max_pos;
+        while (max_pos--)
+            rrb(b);
+    }
+}
+
+
 // Основная функция сортировки методом чанков
 void chunk_sort(t_stack *a, t_stack *b, int size, int chunks)
 {
     int chunk_size = get_chunk_size(size, chunks);
-    int performed = 0;
     
     for (int chunk = 0; chunk < chunks; chunk++)
     {
@@ -146,50 +156,16 @@ void chunk_sort(t_stack *a, t_stack *b, int size, int chunks)
             if (b->size > 1 && b->top->index < (min + max) / 2)
                 rb(b);
             else if (b->size > 1 && b->top->index < b->top->next->index)
-            {
                 sb(b);
-                //ft_printf("sb performed. Stack b size: %d\n", b->size);
-                performed++;
-            }
-            //optimize_swaps_both(a, b);
         }
     }
-    // ft_printf("Performed with sb: %d\n", performed);
     
     // Возвращаем все элементы из b обратно в a
     while (b->size > 0)
     {
-        // Находим максимальный элемент в b
-        t_node *current = b->top;
-        t_node *max = current;
-        int pos = 0;
-        int max_pos = 0;
-        while (current)
-        {
-            if (current->index > max->index)
-            {
-                max = current;
-                max_pos = pos;
-            }
-            current = current->next;
-            pos++;
-        }
-        
-        // Определяем направление вращения
-        if (max_pos <= b->size / 2)
-        {
-            while (max_pos--)
-                rb(b);
-        }
-        else
-        {
-            max_pos = b->size - max_pos;
-            while (max_pos--)
-                rrb(b);
-        }
-        
-        // Перемещаем максимальный элемент обратно в a
+        rotate_to_max(b);
         pa(a, b);
-    }
+    }    
+
 }
 
