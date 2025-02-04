5bb8e4d7da26fc36051a29d6a827cfdf31ef1893
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-02-05 00:00:58 +0100

    -refactor: indexing

--- indexing.c
+++ indexing.c
@@ -1,71 +1,87 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   index.c                                            :+:      :+:    :+:   */
+/*   indexing.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 20:57:55 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/01/31 20:48:17 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/02/04 23:58:50 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "libft/ft_printf.h"
 #include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-// find the minimum value in the stack that does not have an index
-int find_min(t_stack *stack)
-{
-    t_node *current;
-    int min = INT_MAX;
+static int	find_min_unindexed_value(t_stack *stack);
+static void	init_stack_indices(t_stack *stack);
 
-    current = stack->top;
-    while (current)
-    {
-        if (current->index == -1 && current->value < min)
-            min = current->value;
-        current = current->next;
-    }
-    return min;
+/**
+ * @brief Find the minimum value in the stack that does not have an index
+ * @param stack where to search
+ * @return Minimum value
+ */
+static int	find_min_unindexed_value(t_stack *stack)
+{
+	t_node	*current;
+	int		min;
+
+	min = INT_MAX;
+	current = stack->top;
+	while (current)
+	{
+		if (current->index == -1 && current->value < min)
+			min = current->value;
+		current = current->next;
+	}
+	return (min);
 }
 
-// Функция для присвоения индексов
-void assign_sorted_indices(t_stack *stack)
+/**
+ * @brief Initialize the indices of the stack elements
+ * @param stack to initialize
+ */
+static void	init_stack_indices(t_stack *stack)
 {
-    int index = 0;
-    int min;
+	t_node	*current;
 
-    // Инициализируем индексы как -1
-    t_node *current = stack->top;
-    while (current)
-    {
-        current->index = -1;
-        current = current->next;
-    }
+	current = stack->top;
+	while (current)
+	{
+		current->index = -1;
+		current = current->next;
+	}
+}
 
-    // Присваиваем индексы от 0 до size-1
-    while (index < stack->size)
-    {
-        min = find_min(stack);
-        if (min == INT_MAX)
-        {
-            ft_printf("Error: Failed to find minimum during indexing\n");
-            exit(EXIT_FAILURE);
-        }
-        // Присваиваем индекс первому встретившемуся минимальному значению
-        current = stack->top;
-        while (current)
-        {
-            if (current->value == min && current->index == -1)
-            {
-                current->index = index;
-                break;
-            }
-            current = current->next;
-        }
-        index++;
-    }
+/**
+ * @brief Assigns indices to the stack elements in ascending order
+ * @param stack to assign indices to
+ */
+void	assign_sorted_indices(t_stack *stack)
+{
+	int		index;
+	int		min;
+	t_node	*current;
+
+	index = 0;
+	current = stack->top;
+	init_stack_indices(stack);
+	while (index < stack->size)
+	{
+		min = find_min_unindexed_value(stack);
+		if (min == INT_MAX)
+			exit(EXIT_FAILURE);
+		current = stack->top;
+		while (current)
+		{
+			if (current->value == min && current->index == -1)
+			{
+				current->index = index;
+				break ;
+			}
+			current = current->next;
+		}
+		index++;
+	}
 }
