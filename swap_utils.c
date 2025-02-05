e6000b3dd694cd4caa1189ad24ad9314cb22227a
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-02-05 11:34:43 +0100

    -refactor: swap_utils

--- swap_utils.c
+++ swap_utils.c
@@ -6,15 +6,19 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 16:10:03 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/02/02 18:54:43 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:34:11 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/ft_printf.h"
 #include "push_swap.h"
 
-// swap the top two nodes of the stack
-void	swap(t_stack *stack)
+static void	swap(t_stack *stack);
+
+/**
+ * @brief Swap the top two nodes of the stack
+ * @param stack Stack to swap
+ */
+static void	swap(t_stack *stack)
 {
 	int	temp_value;
 	int	temp_index;
@@ -29,21 +33,34 @@ void	swap(t_stack *stack)
 	stack->top->next->index = temp_index;
 }
 
-// Swap A
+/**
+ * @brief Swap the top two nodes of the stack A
+ * @param a Stack A
+ * @param op_list List of operations needed to sort the stacks
+ */
 void	sa(t_stack *a, t_operation **op_list)
 {
 	swap(a);
 	add_operation(op_list, "sa");
 }
 
-// Swap B
+/**
+ * @brief Swap the top two nodes of the stack B
+ * @param b Stack B
+ * @param op_list List of operations needed to sort the stacks
+ */
 void	sb(t_stack *b, t_operation **op_list)
 {
 	swap(b);
 	add_operation(op_list, "sb");
 }
 
-// Swap A и B
+/**
+ * @brief Swap the top two nodes of the stack A and B
+ * @param a Stack A
+ * @param b Stack B
+ * @param op_list List of operations needed to sort the stacks
+ */
 void	ss(t_stack *a, t_stack *b, t_operation **op_list)
 {
 	swap(a);
