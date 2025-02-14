0bffd8f85b73366d7ab4e66af7081b7a4fc87e46
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-02-14 20:03:56 +0100

    -feat: process int_min & int_max

--- indexing.c
+++ indexing.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 20:57:55 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/02/04 23:58:50 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/02/14 20:00:45 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 static int	find_min_unindexed_value(t_stack *stack);
 static void	init_stack_indices(t_stack *stack);
+static int	has_unindexed_elements(t_stack *stack);
 
 /**
  * @brief Find the minimum value in the stack that does not have an index
@@ -26,15 +27,24 @@ static int	find_min_unindexed_value(t_stack *stack)
 {
 	t_node	*current;
 	int		min;
+	int		found;
 
-	min = INT_MAX;
 	current = stack->top;
+	found = 0;
 	while (current)
 	{
-		if (current->index == -1 && current->value < min)
-			min = current->value;
+		if (current->index == -1)
+		{
+			if (!found || current->value < min)
+			{
+				min = current->value;
+				found = 1;
+			}
+		}
 		current = current->next;
 	}
+	if (!found)
+		return (INT_MAX);
 	return (min);
 }
 
@@ -55,6 +65,25 @@ static void	init_stack_indices(t_stack *stack)
 }
 
 /**
+ * @brief Check if there are unindexed elements in the stack
+ * @param stack to check
+ * @return 1 if there are unindexed elements, 0 otherwise
+ */
+static int	has_unindexed_elements(t_stack *stack)
+{
+	t_node	*current;
+
+	current = stack->top;
+	while (current)
+	{
+		if (current->index == -1)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
+
+/**
  * @brief Assigns indices to the stack elements in ascending order
  * @param stack to assign indices to
  */
@@ -69,9 +98,9 @@ void	assign_sorted_indices(t_stack *stack)
 	init_stack_indices(stack);
 	while (index < stack->size)
 	{
-		min = find_min_unindexed_value(stack);
-		if (min == INT_MAX)
+		if (!has_unindexed_elements(stack))
 			exit(EXIT_FAILURE);
+		min = find_min_unindexed_value(stack);
 		current = stack->top;
 		while (current)
 		{
